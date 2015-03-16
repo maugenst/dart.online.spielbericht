15d1ac300851d99be07e28506a26105efce5de72
Marius Augenstein <marius.augenstein@sap.com>
2015-03-16 15:39:08 +0100

    Preparation for project beeing served on github
    changed httpserver.js to enable a local development and added gitignore
    file to ignore node modules

--- httpServer.js
+++ httpServer.js
@@ -1,7 +1,12 @@
 var express = require("express");
+var serveIndex = require('serve-index');
 var hostname = "localhost";
 var port = 9000;
 var app = express();
+
+
+app.use(express.static(__dirname + "/"))
+app.use('/spielbericht', serveIndex(__dirname + '/spielbericht'));
 
 process.on('exit', function() {
 	console.log("Closing");
