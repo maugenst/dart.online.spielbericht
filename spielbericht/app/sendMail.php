c0fc4a4a94c8ae401153d49028b51f3ab0c2c44e
Marius Augenstein <marius.augenstein@sap.com>
2015-03-24 17:30:48 +0100

    version 1.0

--- spielbericht/app/sendMail.php
+++ spielbericht/app/sendMail.php
@@ -3,56 +3,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-function mail_att($to,$subject,$message,$anhang) 
-   { 
-   $absender = "BDL Online Spielplan"; 
-   $absender_mail = "bdlonlinespielplan@gmail.com"; 
-   $reply = "bdlonlinespielplan@gmail.com"; 
+require_once 'class.phpmailer.php';
 
-   $mime_boundary = "-----=" . md5(uniqid(mt_rand(), 1)); 
-
-   $header  ="From:".$absender."<".$absender_mail.">\n"; 
-   $header .= "Reply-To: ".$reply."\n"; 
-
-   $header.= "MIME-Version: 1.0\r\n"; 
-   $header.= "Content-Type: multipart/mixed;\r\n"; 
-   $header.= " boundary=\"".$mime_boundary."\"\r\n"; 
-
-   $content = "This is a multi-part message in MIME format.\r\n\r\n"; 
-   $content.= "--".$mime_boundary."\r\n"; 
-   $content.= "Content-Type: text/html charset=\"iso-8859-1\"\r\n"; 
-   $content.= "Content-Transfer-Encoding: 8bit\r\n\r\n"; 
-   $content.= $message."\r\n"; 
-
-   //$anhang ist ein Mehrdimensionals Array 
-   //$anhang enthält mehrere Dateien 
-   if(is_array($anhang) AND is_array(current($anhang))) { 
-      foreach($anhang AS $dat) { 
-         $data = chunk_split(base64_encode($dat['data'])); 
-         $content.= "--".$mime_boundary."\r\n"; 
-         $content.= "Content-Disposition: attachment;\r\n"; 
-         $content.= "\tfilename=\"".$dat['name']."\";\r\n"; 
-         $content.= "Content-Length: .".$dat['size'].";\r\n"; 
-         $content.= "Content-Type: ".$dat['type']."; name=\"".$dat['name']."\"\r\n"; 
-         $content.= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-         $content.= $data."\r\n"; 
-      } 
-      $content .= "--".$mime_boundary."--";  
-   } else { //Nur 1 Datei als Anhang 
-      $data = chunk_split(base64_encode($anhang['data'])); 
-      $content.= "--".$mime_boundary."\r\n"; 
-      $content.= "Content-Disposition: attachment;\r\n"; 
-      $content.= "\tfilename=\"".$anhang['name']."\";\r\n"; 
-      $content.= "Content-Length: .".$anhang['size'].";\r\n"; 
-      $content.= "Content-Type: ".$anhang['type']."; name=\"".$anhang['name']."\"\r\n"; 
-      $content.= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-      $content.= $data."\r\n"; 
-   }  
-       
-   if(@mail($to, $subject, $content, $header)) return true; 
-   else return false; 
-}
-
+$mail = new PHPMailer(true); //defaults to using php "mail()"; the true param means it will throw exceptions on errors, which we need to catch
 
 function randString($length) {
     $char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -66,7 +19,7 @@ function randString($length) {
 $randomStringPart = randString(5);
 
 $fileNameBild = 'onlineSpielberichtBild_'.$randomStringPart.'.png';
-$fileNameErgebnisJSON = 'ergebnis_'.$randomStringPart.'.erg';
+$fileNameErgebnisJSON = 'ergebnis_'.$randomStringPart.'.json';
 
 include('SimpleImage.php');
 $image = new SimpleImage();
@@ -75,34 +28,60 @@ $image->resizeToWidth(1000);
 $image->save($fileNameBild);
 
 $json = $_POST['ergebnisJSON'];
+$css = $_POST['emailCSSStyles'];
+$table = $_POST['emailSummaryTable'];
 
 file_put_contents($fileNameErgebnisJSON, $json);
 
-$pfad = array(); 
-$pfad[] = $fileNameBild; 
-$pfad[] = $fileNameErgebnisJSON; 
-
 $anhang = array(); 
-foreach($pfad AS $name) { 
-  
-   $name = basename($name); 
-   $size = filesize($name); 
-   $data = implode("",file($name)); 
+$anhang[] = $fileNameBild; 
+$anhang[] = $fileNameErgebnisJSON; 
 
-   if(function_exists("mime_content_type")) 
-      $type = mime_content_type($name); 
-   else 
-      $type = "application/octet-stream"; 
-    $anhang[] = array("name"=>$name, "size"=>$size, "type"=>$type, "data"=>$data); 
+$mailBody =  '<html>';
+$mailBody .= '<head>';
+$mailBody .= '  <style>';
+$mailBody .= $css;
+$mailBody .= '  </style>';
+$mailBody .= '</head>';
+$mailBody .= '<body>';
+$mailBody .= '  <h1>Spielberichtsbogen der BDL</h1><br>';
+$mailBody .= '  Hier das Ergebnis der Partie:';
+$mailBody .= $table;
+$mailBody .= '</body>';
+$mailBody .= '</html>';
+
+$okhtml =  '<html>';
+$okhtml .= '<head>';
+$okhtml .= '    <meta charset="UTF-8">';
+$okhtml .= '    <meta http-equiv="refresh" content="3;url=http://bdl-online-spielplan.lima-city.de/">';
+$okhtml .= '    <title>BDL Online Spielbericht</title>';
+$okhtml .= '</head>';
+$okhtml .= '<body>';
+$okhtml .= '  <h1>Vielen Dank. Der Spielbericht wurde zur BDL zur Bearbeitung gesandt. Du wirst nun zur Startseite umgeleitet.</h1>';
+$okhtml .= '</body>';
+$okhtml .= '</html>';
+
+try {
+  $mail->AddAddress('Marius.Augenstein@gmail.com','Marius Augenstein');
+  $mail->SetFrom('bdlonlinespielplan@gmail.com', 'BDL Online Spielbericht');
+  $mail->AddReplyTo('bdlonlinespielplan@gmail.com', 'BDL Online Spielbericht');
+  $mail->Subject = "BDL Online Spielberichtsbogen";
+  $mail->AltBody = ""; // optional - MsgHTML will create an alternate automatically
+  $mail->MsgHTML($mailBody);
+
+  $mail->AddAttachment($fileNameErgebnisJSON); // attachment
+  $mail->AddAttachment($fileNameBild); // attachment
+  
+  $mail->Send();
+  echo $okhtml;
+} catch (phpmailerException $e) {
+  echo $e->errorMessage(); //Pretty error messages from PHPMailer
+} catch (Exception $e) {
+  echo $e->getMessage(); //Boring error messages from anything else!
 }
 
-$mailBody =  '<h1>Spielberichtsbogen der BDL<h1><br>\n';
-$mailBody .= 'Hier das Ergebnis ...';
-$mailBody .= '<pre><code>' . $json . '</code></pre>';
+unlink($fileNameBild); 
+unlink($fileNameErgebnisJSON);
 
-$mailRcpt = "Marius.Augenstein@gmail.com";
-$ret = mail_att($mailRcpt,"Spielbericht",$mailBody,$anhang);
-echo $ret ? "An email has been successfully been sent to: ".$mailRcpt : "not ok";
 
-?>	
-	
+?>
