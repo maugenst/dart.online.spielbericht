52b4708898eb3cf54c47205d7e32cc5d3f17204c
Marius Augenstein <marius.augenstein@sap.com>
2015-03-29 12:20:45 +0200

    Added copyright and impressum

--- spielbericht/app/sendMail.php
+++ spielbericht/app/sendMail.php
@@ -24,7 +24,7 @@ $fileNameErgebnisJSON = 'ergebnis_'.$randomStringPart.'.json';
 include('SimpleImage.php');
 $image = new SimpleImage();
 $image->load($_FILES['onlineSpielberichtBild']['tmp_name']);
-$image->resizeToWidth(1000);
+$image->resizeToWidth(2000);
 $image->save($fileNameBild);
 
 $json = $_POST['ergebnisJSON'];
@@ -47,23 +47,33 @@ $mailBody .= '<body>';
 $mailBody .= '  <h1>Spielberichtsbogen der BDL</h1><br>';
 $mailBody .= '  Hier das Ergebnis der Partie:';
 $mailBody .= $table;
-$mailBody .= '<p>&copy; <a href="mailto:marius.augenstein@gmail.com">Marius Augenstein</a> - Badische Dart Liga - BDL - http://www.badischedartliga.de</p>';
+$mailBody .= '<p>&copy; <b><a href="mailto:marius.augenstein@gmail.com">Marius Augenstein (2015)</a></b> (<a href="http://bdl-online-spielplan.lima-city.de/impressum.html">Impressum</a>) <br>';
+$mailBody .= '<a href="http://www.badischedartliga.de">Badische Dart Liga</a> | <a href="http://www.badischedartliga.de">BDL Online Spielbericht</a>';
 $mailBody .= '</body>';
 $mailBody .= '</html>';
 
 $okhtml =  '<html>';
 $okhtml .= '<head>';
-$okhtml .= '    <meta charset="UTF-8">';
+$okhtml .= '    <meta charset="utf-8">';
+$okhtml .= '    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+$okhtml .= '    <link rel="stylesheet" href="styles/main.css">';
 $okhtml .= '    <meta http-equiv="refresh" content="3;url=http://bdl-online-spielplan.lima-city.de/">';
 $okhtml .= '    <title>BDL Online Spielbericht</title>';
 $okhtml .= '</head>';
 $okhtml .= '<body>';
 $okhtml .= '  <h1>Vielen Dank. Der Spielbericht wurde zur BDL zur Bearbeitung gesandt. Du wirst nun zur Startseite umgeleitet.</h1>';
+$okhtml .= '<p>&copy; <b><a href="mailto:marius.augenstein@gmail.com">Marius Augenstein (2015)</a></b> (<a href="http://bdl-online-spielplan.lima-city.de/impressum.html">Impressum</a>) <br>';
+$okhtml .= '<a href="http://www.badischedartliga.de">Badische Dart Liga</a> | <a href="http://www.badischedartliga.de">BDL Online Spielbericht</a>';
 $okhtml .= '</body>';
 $okhtml .= '</html>';
 
 try {
-  $mail->AddAddress('Marius.Augenstein@gmail.com','Marius Augenstein');
+  $mail->AddAddress('bdlonlinespielplan@gmail.com', 'BDL Online Spielbericht');
+  $mail->AddAddress('spielleiter@badischedartliga.de', 'Spielleiter BDL');
+  $mail->AddAddress('bdl@bwdv.de','BDL@BWDV');
+  $mail->AddCC('odom3003@googlemail.com ','Dominik Boss');
+  $mail->AddCC('jb@jankovsky.de', 'Jochen Becker');
+  $mail->AddCC('Marius.Augenstein@gmail.com','Marius Augenstein');
   $mail->SetFrom('bdlonlinespielplan@gmail.com', 'BDL Online Spielbericht');
   $mail->AddReplyTo('bdlonlinespielplan@gmail.com', 'BDL Online Spielbericht');
   $mail->Subject = "BDL Online Spielberichtsbogen";
