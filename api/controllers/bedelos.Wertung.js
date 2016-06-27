eccf5efc92b40290b62c2fcf5200f55ec92c459b
Marius Augenstein <marius.augenstein@sap.com>
2016-06-27 13:28:51 +0200

    added possibillity for bdl manager to reset games

--- api/controllers/bedelos.Wertung.js
+++ api/controllers/bedelos.Wertung.js
@@ -9,7 +9,22 @@ var nodemailer = require("nodemailer");
 var jsonfile = require('jsonfile');
 jsonfile.spaces = 4;
 var walker = require('walker');
+var uid = require('../helpers/UID');
 var _ = require('lodash');
+var url = require('url');
+
+
+function calcLigaFromGameId(gameId) {
+    if (gameId.startsWith('kls')) {
+        return 'klsued';
+    } else if (gameId.startsWith('kln')) {
+        return 'klnord';
+    } else if (gameId.startsWith('bzli')) {
+        return 'bzLiga';
+    } else if (gameId.startsWith('ol')) {
+        return 'oberliga';
+    }
+}
 
 function updateStatistics(oParameters){
     // Updating statistics...
@@ -86,39 +101,85 @@ function updateTable(oParameters){
     jsonfile.writeFileSync(sTableFile, oTabelle);
 }
 
+function gameDetails(gameId, oSpielplan, liga){
+    for (var spieltag in oSpielplan[liga].vr) {
+        for (var spiel in oSpielplan[liga].vr[spieltag]) {
+            if (oSpielplan[liga].vr[spieltag][spiel].id === gameId) {
+                return oSpielplan[liga].vr[spieltag][spiel];
+            }
+        }
+    }
+    for (var spieltag in oSpielplan[liga].rr) {
+        for (var spiel in oSpielplan[liga].rr[spieltag]) {
+            if (oSpielplan[liga].rr[spieltag][spiel].id === gameId) {
+                return oSpielplan[liga].rr[spieltag][spiel];
+            }
+        }
+    }
+    return {};
+}
+function setResult(oCurrentResult, heim, gast) {
+    oCurrentResult.summary.heim.sets = heim;
+    oCurrentResult.summary.gast.sets = gast;
+}
+
 function wertung (req, res) {
     try {
+        var uniqueGameId = uid.generate();
         var sPath = path.resolve(config.get("bedelos.datapath"));
-        var sTablesPath = path.resolve('/tabellen/');
+        var sTablesPath = path.resolve(sPath + '/tabellen/');
         var oTeams = require(sPath + '/Teams.json');
-
+        var oSpielplan = require(sPath + '/Spielplan.json');
         var sGameId = req.swagger.params.gameId.originalValue;
         var oCurrentResult = jsonfile.readFileSync(sPath + '/Wertung.json');
-        var newName = path.resolve(sPath + '/ergebnisse/' + path.basename(file).substring(7));
-        /*fs.renameSync(file, newName);
+        var liga = calcLigaFromGameId(sGameId);
 
-        walker(sPath + '/inbox').on('file', function(file, stat) {
-            if (path.basename(file).indexOf(sGameId) === 0) {
-                var liga = calcLigaFromFilename(file);
+        var oGameDetails = gameDetails(sGameId, oSpielplan, liga);
+        oCurrentResult.heim = oGameDetails.heim;
+        oCurrentResult.gast = oGameDetails.gast;
 
-                updateStatistics({
-                    currentResults: oCurrentResult,
-                    liga: liga,
-                    pathToStatisticsFiles: sStatisticsPath
-                });
-                updateTable({
-                    currentResults: oCurrentResult,
-                    liga: liga,
-                    pathToTablesFiles: sTablesPath
-                });
 
-                // So if everything is fine ... rename / move the file to results folder
-                var newName = path.resolve(sPath + '/ergebnisse/' + path.basename(file).substring(7));
-                fs.renameSync(file, newName);
-            }
-        }).on('end', function() {
-            res.redirect('/bedelos/inbox');
-        });*/
+        switch(req.swagger.params.wertung.originalValue.value) {
+            case '0': setResult(oCurrentResult, 0, 0);
+                break;
+            case '1': setResult(oCurrentResult, 0, 12);
+                break;
+            case '2': setResult(oCurrentResult, 1, 11);
+                break;
+            case '3': setResult(oCurrentResult, 2, 10);
+                break;
+            case '4': setResult(oCurrentResult, 3, 9);
+                break;
+            case '5': setResult(oCurrentResult, 4, 8);
+                break;
+            case '6': setResult(oCurrentResult, 5, 7);
+                break;
+            case '7': setResult(oCurrentResult, 6, 6);
+                break;
+            case '8': setResult(oCurrentResult, 7, 5);
+                break;
+            case '9': setResult(oCurrentResult, 8, 4);
+                break;
+            case '10': setResult(oCurrentResult, 9, 3);
+                break;
+            case '11': setResult(oCurrentResult, 10, 2);
+                break;
+            case '12': setResult(oCurrentResult, 11, 1);
+                break;
+            case '13': setResult(oCurrentResult, 12, 0);
+                break;
+        }
+
+        var newName = path.resolve(sPath + '/inbox/' + uniqueGameId + "_" + sGameId + ".json");
+        jsonfile.writeFileSync(newName, oCurrentResult);
+        updateTable({
+            currentResults: oCurrentResult,
+            liga: liga,
+            pathToTablesFiles: sTablesPath
+        });
+
+        var referrerPath = url.parse(req.headers.referer).path;
+        res.redirect(referrerPath);
 
     } catch (error) {
         res.status(500).send("Error: " + error.stack.replace('/\n/g', '<br>'));
