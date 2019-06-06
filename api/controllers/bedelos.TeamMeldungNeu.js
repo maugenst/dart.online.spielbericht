2380bdd3ca09d68cdae24bb9e701ce3c1f793165
Marius Augenstein <marius.augenstein@sap.com>
2019-06-06 11:15:55 +0200

    new Team Meldung

---
+++ api/controllers/bedelos.TeamMeldungNeu.js
@@ -0,0 +1,125 @@
+'use strict';
+
+var path = require('path');
+var config = require('config');
+var os = require('os');
+var fs = require('fs');
+var qs = require('querystring');
+var pug = require('pug');
+var nodemailer = require("nodemailer");
+var logger = require('../helpers/Logger');
+var session = require('../helpers/Session');
+var Spielplan = require('../helpers/Spielplan');
+
+var sPath = path.resolve(config.get("bedelos.datapath"));
+var oTeams = require(sPath + '/Teams.json');
+var oVereine = require(sPath + '/Vereine.json');
+
+function get(req, res) {
+
+  try {
+    var oSessionData = session.get(req.cookies.BDL_SESSION_TOKEN);
+
+    if (!oSessionData) {
+      res.cookie('BDL_SESSION_REDIRECT', req.url);
+      res.redirect("/bedelos/login");
+      return;
+    }
+
+    if (oSessionData.username === config.get("bedelos.adminuser")) {
+      res.status(200).send(pug.renderFile("api/views/authorizederror.jade"));
+      return;
+    }
+
+    const teamId = oSessionData.username;
+    const team = oTeams[teamId];
+    const verein = oVereine[team.verein];
+
+    var html = pug.renderFile("api/views/teammeldungNeu.pug", {
+      pretty: true,
+      verein: verein,
+      team: team
+    });
+
+    res.status(200).send(html);
+
+  } catch (error) {
+    res.status(500).send("Error: " + error.stack.replace('/\n/g', '<br>'));
+
+    logger.log.debug(error.stack);
+  }
+
+
+}
+
+function post(req, res) {
+
+  try {
+    var oSessionData = session.get(req.cookies.BDL_SESSION_TOKEN);
+
+    if (!oSessionData) {
+      res.cookie('BDL_SESSION_REDIRECT', req.url);
+      res.redirect("/bedelos/login");
+      return;
+    }
+
+    if (oSessionData.username === config.get("bedelos.adminuser")) {
+      res.status(200).send(pug.renderFile("api/views/authorizederror.jade"));
+      return;
+    }
+
+    var body = pug.renderFile("api/views/teammeldungNeuBody.pug", {
+      pretty: true,
+      data: req.body
+    });
+
+    var mail = pug.renderFile("api/views/teammeldungNeuMail.pug", {
+      pretty: true,
+      data: req.body
+    });
+
+    var transporter = nodemailer.createTransport({
+      sendmail: true,
+      newline: 'unix',
+      path: '/usr/sbin/sendmail'
+    });
+    var aMailTo = [];
+    var aMailCC = [];
+
+    aMailTo.push('Spielleiter BDL <spielleiter@badischedartliga.de>');
+    aMailTo.push('Praesident BDL <praesident@badischedartliga.de>');
+    aMailTo.push('Vizepraesident BDL <vizepraesident@badischedartliga.de>');
+    aMailTo.push('Schriftfuehrer BDL <schriftfuehrer@badischedartliga.de>');
+
+    aMailCC.push(`Teamkapitän <${req.body.TeamkapitaenEmail}>`);
+
+      var mailOptions = {
+        from: 'BDL Online Team Meldung <teammeldung@badischedartliga.de>',
+        to: '\'' + aMailTo.join(', ') + '\'',
+        cc: '\'' + aMailCC.join(', ') + '\'',
+
+        subject: 'BDL Online Team Meldung',
+        html: mail
+      };
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        throw new Error('Failed to send email: '  + error.stack);
+      }
+      res.status(200).send(body);
+    });
+
+  } catch (error) {
+    res.status(500).send("Error: " + error.stack.replace('/\n/g', '<br>'));
+
+    logger.log.debug(error.stack);
+  }
+
+
+}
+
+module.exports = {
+    get: get,
+    post: post
+};
